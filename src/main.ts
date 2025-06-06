29dfa456c7807c6d55f9c3adb758f44706fe611a
KawNguyen <khoanguyencool12@gmail.com>
2025-06-06 11:26:14 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,7 @@ async function bootstrap() {
   app.enableCors({
     origin: ['http://localhost:3000', 'http://localhost:3001'],
     credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
   const config = new DocumentBuilder()
