a543cd4931f92e9f4714fa4a90a53ce45412c58e
KawNguyen <khoanguyencool12@gmail.com>
2025-06-19 15:33:57 +0700

    update

--- src/brand/brand.module.ts
+++ src/brand/brand.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { BrandService } from './brand.service';
 import { BrandController } from './brand.controller';
-import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
 
 @Module({
-  imports: [CloudinaryModule],
   providers: [BrandService],
   controllers: [BrandController],
 })
